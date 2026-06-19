4784f3df74ec32f4f949118c29e2ca84862d7579
IgsonJr1 <igson.com@hotmail.com>
2026-06-19 11:53:25 -0400

    Update PluginServiceRegistrator.cs

--- src/MediaNotesPro2.0/PluginServiceRegistrator.cs
+++ src/MediaNotesPro2.0/PluginServiceRegistrator.cs
@@ -1,5 +1,14 @@
+using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
 using MediaNotesPro.Storage;
 using Microsoft.Extensions.DependencyInjection;
+
 namespace MediaNotesPro;
-public sealed class PluginServiceRegistrator:IPluginServiceRegistrator { public void RegisterServices(IServiceCollection s)=>s.AddSingleton<IMediaNoteStore,JsonMediaNoteStore>(); }
+
+public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(IServiceCollection services, IServerApplicationHost applicationHost)
+    {
+        services.AddSingleton<IMediaNoteStore, JsonMediaNoteStore>();
+    }
+}
